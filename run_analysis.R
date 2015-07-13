e1252891b1348f763f092e3da4120b9696824d54
Marco Wirth <github@wirth.it>
2015-07-13 10:40:26 +0200

    "Getting and Cleaning Data Course Project" Adding the run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,76 @@
+# 0) load and install required packages and prepare data ---------
+##load packages
+if(!require(data.table)) {
+      install.packages("data.table")
+      library(data.table)
+}
+
+## checking wether the file already exists, if not download the zip-file
+if (file.exists("getdata-projectfiles-UCI HAR Dataset.zip")) {
+      print("data for the project exists")
+} else {
+      print("data for the project is beeing downloaded")
+      download.file(url       = "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
+                    destfile  = "getdata-projectfiles-UCI HAR Dataset.zip")
+}
+
+## extract all files in the zip-file
+unzip("getdata-projectfiles-UCI HAR Dataset.zip")
+
+
+# 1) merge the training and the test set to create one data set -----------
+test.x <- data.table(read.table(file = "./UCI HAR Dataset/test/X_test.txt"))
+test.y <- data.table(read.table(file = "./UCI HAR Dataset/test/y_test.txt"))
+test.subject <- data.table(read.table(file = "./UCI HAR Dataset/test/subject_test.txt"))
+
+train.x <- data.table(read.table(file = "./UCI HAR Dataset/train/X_train.txt"))
+train.y <- data.table(read.table(file = "./UCI HAR Dataset/train/y_train.txt"))
+train.subject <- data.table(read.table(file = "./UCI HAR Dataset/train/subject_train.txt"))
+
+merge.x <- rbind(test.x, train.x)
+merge.y <- rbind(test.y, train.y)
+merge.subject <- rbind(test.subject, train.subject)
+
+
+# 2) extracts only the measurements on the mean and standard deviation --------
+features <- data.table(read.table("./UCI HAR Dataset/features.txt"))
+setnames(features, c("V1", "V2"), c("id", "description"))
+features.mean <- features[description %like% "mean"]
+features.std <- features[description %like% "std"]
+
+setnames(merge.x, as.character(features$description))
+merge.x.mean.std <- merge.x[, c(features.mean$id, features.std$id), with = FALSE]
+
+
+# 3) use descriptive activity names to name the activities in the data set --------
+setnames(merge.y, "Activity")
+activity.labels <- data.table(read.table("UCI HAR Dataset/activity_labels.txt", header = FALSE))
+merge.y$activity <- factor(merge.y$Activity, labels = activity.labels$V2)
+
+
+# 4) appropriately label the data set with descriptive variable names --------
+setnames(merge.y, "Activity")
+setnames(merge.subject, "Subject")
+
+data <- cbind (merge.x.mean.std, merge.y, merge.subject)
+
+data.names <- names(data)
+data.names <- gsub("Acc", "Accelerometer", data.names)
+data.names <- gsub("^t", "Time", data.names)
+data.names <- gsub("^f", "Frequency", data.names)
+data.names <- gsub("Gyro", "Gyroscope", data.names)
+data.names <- gsub("BodyBody", "Body", data.names)
+data.names <- gsub("Mag", "Magnitude", data.names)
+data.names <- gsub("^t", "Time", data.names)
+data.names <- gsub("^f", "Frequency", data.names)
+data.names <- gsub("tBody", "TimeBody", data.names)
+data.names <- gsub("-mean\\(\\)", "Mean", data.names, ignore.case = TRUE)
+data.names <- gsub("-std\\(\\)", "STD", data.names, ignore.case = TRUE)
+data.names <- gsub("-freq()", "Frequency", data.names, ignore.case = TRUE)
+data.names <- gsub("angle", "Angle", data.names)
+data.names <- gsub("gravity", "Gravity", data.names)
+setnames(data, data.names)
+
+
+# 5) create a second, independent tidy data set with the average of each variable for each activity and each subject --------
+data.tidy <- NULL
